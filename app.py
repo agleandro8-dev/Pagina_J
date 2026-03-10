abce68bfbbfb1d4e3183afb4955680ff0091fd9b
agleandro8-dev <agleandro8@gmail.com>
2026-03-09 19:46:02 -0500

    Configuracion para despliegue en Render.com

--- app.py
+++ app.py
@@ -15,4 +15,4 @@ def principal():
 if __name__ == "__main__":
     import os
     port = int(os.environ.get("PORT", 5000))
-    app.run(host='0.0.0.0', port=port, debug=True)
+    app.run(host='0.0.0.0', port=port, debug=False)
